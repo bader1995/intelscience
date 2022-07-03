dda7f03c7f5a141b31c5f11ab61ada7a0380b96f
unknown <bader1995>
2022-07-03 22:48:11 +0100

    Modifications

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -7,16 +7,18 @@ export default function HomeScreen({ navigation }) {
 
   const [cours, setCours] = useState(null)
 
-  const getCours = async () => {
-    try {
-     const response = await fetch('https://lyoumapress.com/api_intelscience/index.php');
-     const json = await response.json();
+  const getCours = async () =>
+  {
+    try
+    {
+      const response = await fetch('https://lyoumapress.com/api_intelscience/index.php');
+      const json = await response.json();
 
-     setCours(json)
+      setCours(json)
 
-   } catch (error) {
-     console.error(error);
-   }
+    } catch (error) {
+      console.error(error);
+    }
  }
 
  useEffect(() => {
@@ -27,22 +29,6 @@ export default function HomeScreen({ navigation }) {
     <ScrollView style={styles.container}>
         <View style={styles.insideContainer}>
             <Text style={{ alignSelf: "center", fontSize: 18, marginBottom: 20 }}>Science de la vie et de la terre</Text>
-            <View style={{ display: 'flex', flexDirection: 'row' }}>
-              <DetailCardComponent title="La planète Terre, l’environnement et l’action humaine" onPress={() => navigation.navigate("Detail")} />
-              <DetailCardComponent title="Le corps humain et la santé" onPress={() => navigation.navigate("Detail")} />
-            </View>
-            <View style={{ display: 'flex', flexDirection: 'row' }}>
-              <DetailCardComponent title="La planète Terre, l’environnement et l’action humaine" onPress={() => navigation.navigate("Detail")} />
-              <DetailCardComponent title="Le vivant et son évolution" onPress={() => navigation.navigate("Detail")} />
-            </View>
-            <View style={{ display: 'flex', flexDirection: 'row' }}>
-              <DetailCardComponent title="La planète Terre, l’environnement et l’action humaine" onPress={() => navigation.navigate("Detail")} />
-              <DetailCardComponent title="Le corps humain et la santé" onPress={() => navigation.navigate("Detail")} />
-            </View>
-            <View style={{ display: 'flex', flexDirection: 'row' }}>
-              <DetailCardComponent title="La planète Terre, l’environnement et l’action humaine" onPress={() => navigation.navigate("Detail")} />
-              <DetailCardComponent title="Le corps humain et la santé" onPress={() => navigation.navigate("Detail")} />
-            </View>
             <View style={{ display: 'flex', flexDirection: 'row' }}>
               <DetailCardComponent title="La planète Terre, l’environnement et l’action humaine" onPress={() => navigation.navigate("Detail")} />
               <DetailCardComponent title="Le corps humain et la santé" onPress={() => navigation.navigate("Detail")} />
